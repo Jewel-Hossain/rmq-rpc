38a8375cbb55e652c91982fe94846879e6a328eb
Jewel Hossain <xem.information@gamil.com>
2023-02-03 04:28:12 +0600

    :bookmark: init

---
+++ client/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Concurrent;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var rpcClient = new RpcClient();
+
+// Console.WriteLine(" [x] Requesting fib(30)");
+var response = rpcClient.Call("{\"inpu\":9}");
+
+Console.WriteLine(" [.] Got = '{0}'", response);
+rpcClient.Close();
+
+
+
+public class RpcClient
+{
+    private readonly IConnection connection;
+    private readonly IModel channel;
+    private readonly string replyQueueName;
+    private readonly EventingBasicConsumer consumer;
+    private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
+    private readonly IBasicProperties props;
+
+    public  RpcClient()
+    {
+        // var factory = new ConnectionFactory() { HostName = "localhost" ,Port = 5672 , UserName = "user",Password = "mypass" };
+        var factory = new ConnectionFactory() { HostName = "18.138.164.11", Port = 5672, UserName = "admin", Password = "admin2023" };
+
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        replyQueueName = channel.QueueDeclare().QueueName;
+        Console.WriteLine($"Reply queue : {replyQueueName}");
+        consumer = new EventingBasicConsumer(channel);
+
+        props = channel.CreateBasicProperties();
+        var correlationId = Guid.NewGuid().ToString();
+        props.CorrelationId = correlationId;
+        props.ReplyTo = replyQueueName;
+        props.MessageId = "get";
+
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var response = Encoding.UTF8.GetString(body);
+            if (ea.BasicProperties.CorrelationId == correlationId)
+            {
+                respQueue.Add(response);
+            }
+        };
+
+        channel.BasicConsume(
+            consumer: consumer,
+            queue: replyQueueName,
+            autoAck: true);
+    }//constructor
+
+    public string Call(string message)
+    {
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(
+            exchange: "",
+            routingKey: "central_queue",
+            basicProperties: props,
+            body: messageBytes);
+
+        return respQueue.Take();
+    }//func
+
+    public void Close()
+    {
+        connection.Close();
+    }//func
+
+}//class
---
+++ server/Program.cs
@@ -0,0 +1,109 @@
+﻿// In the name of Allah
+
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "user", Password = "mypass" };
+var factory = new ConnectionFactory() { HostName = "18.138.164.11", Port = 5672, UserName = "admin", Password = "admin2023" };
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+    channel.QueueDeclare(
+        queue: "restaurant_queue",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+        );
+
+channel.BasicQos(
+    prefetchSize: 0,
+    prefetchCount: 1,
+    global: false
+    );
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(
+    queue: "restaurant_queue",
+    autoAck: false,
+    consumer: consumer
+    );
+
+Console.WriteLine(" [x] Awaiting RPC requests");
+
+consumer.Received += (model, ea) =>
+{
+
+    //response
+    string response = string.Empty;
+
+    var body = ea.Body.ToArray();
+    var props = ea.BasicProperties;
+    var route = props.MessageId;
+
+    //reply
+    var replyProps = channel.CreateBasicProperties();
+    replyProps.CorrelationId = props.CorrelationId;
+
+    try
+    {
+        var request_body = Encoding.UTF8.GetString(body);
+        Parameter? parameter = JsonSerializer.Deserialize<Parameter>(request_body);
+        if (parameter is null) response = "Invalid request";
+        int input = parameter!.input;
+
+        Console.WriteLine($"route : {route}");
+        Console.WriteLine(" [.] fib({0})", request_body);
+
+        if (route == "fib")
+        {
+            response = fib(input).ToString();
+        }//if
+        else
+        {
+            response = "Not found route!";
+        }//else
+
+    }//try
+    catch (Exception e)
+    {
+        Console.WriteLine(" [.] " + e.Message);
+        response = e.Message;
+    }//catch
+    finally
+    {
+        var responseBytes = Encoding.UTF8.GetBytes(response);
+
+        channel.BasicPublish(
+            exchange: "",
+            routingKey: props.ReplyTo,
+            basicProperties: replyProps,
+            body: responseBytes
+            );
+        channel.BasicAck(
+            deliveryTag: ea.DeliveryTag,
+            multiple: false
+          );
+    }//finally
+};
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+int fib(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}//func
+
+
+class Parameter
+{
+    public int input { get; set; }
+}//class
